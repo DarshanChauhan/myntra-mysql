445114f5f0830181ed42e5ee73cf071d808e1e1c
Darshan <darshanchauhan7866@gmail.com>
2022-06-03 11:11:25 +0530

    changes

--- schema/addressSchema.js
+++ schema/addressSchema.js
@@ -1,10 +1,10 @@
 const Joi = require("joi");
 
-const addressSchema = Joi.object({
+const addressSchema1 = Joi.object({
   homeAddress: Joi.string().min(9).max(200).lowercase().required(),
   customerId: Joi.required(),
 });
 
 module.exports = {
-  addressSchema,
+  addressSchema1,
 };
